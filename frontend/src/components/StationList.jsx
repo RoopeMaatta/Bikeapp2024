ed3b868f2025f4998fcf0ce86c0ea3957ebaf004
Roope <129190080+RoopeMaatta@users.noreply.github.com>
2023-11-09 18:53:37 +0000

    StaionsList include ArrowForwardIosIcon

--- frontend/src/components/StationList.jsx
+++ frontend/src/components/StationList.jsx
@@ -4,6 +4,8 @@ import Typography from '@mui/material/Typography';
 import Paper from '@mui/material/Paper';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 
 const StationList = ({ stations }) => {
   return (
@@ -12,13 +14,20 @@ const StationList = ({ stations }) => {
         Stations
       </Typography>
       <Paper elevation={1} sx={{ borderRadius: '8px', padding: '16px' }}>
-        <List>
+        <List sx={{ padding: 0 }}>
           {stations.map((station) => (
-            <ListItem key={station.id} disablePadding>
-              <Link to={`/stations/${station.id}`} style={{ textDecoration: 'none', color: 'inherit', width: '100%' }}>
+            <ListItem
+              key={station.id}
+              disablePadding
+              sx={{ display: 'flex', justifyContent: 'space-between', padding: '8px 16px' }}
+            >
+              <Link to={`/stations/${station.id}`} style={{ textDecoration: 'none', color: 'inherit', display: 'flex', alignItems: 'center', width: '100%', justifyContent: 'space-between' }}>
                 <Typography variant="body1" component="p">
                   {station.station_name}
                 </Typography>
+                <ListItemIcon sx={{ minWidth: 'auto' }}>
+                  <ArrowForwardIosIcon fontSize="inherit" sx={{ fontSize: '0.8rem', color: 'rgba(0, 0, 0, 0.4)' }} />
+                </ListItemIcon>
               </Link>
             </ListItem>
           ))}
